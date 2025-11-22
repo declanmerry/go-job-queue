7cfe84927ac6c47a29e38fa68c3fdce6ab6c640c
declanmerry <declan.merry@gmail.com>
2025-11-22 21:32:21 +0000

    added Dead Letter Queue

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,5 +1,3 @@
 package version
 
-// Version holds the current application version.
-// You can update this manually or inject it at build time using -ldflags.
-var Version = "0.0.1.0"
+var Version = "0.0.1.2"
